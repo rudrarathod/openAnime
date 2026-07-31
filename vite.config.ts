d5d36d5702628695f9bfb324fffd6692f468b12b
Rudra Rathod <rudrarathod738@gmail.com>
2026-08-01 00:43:06 +0530

    chore: update default application title to openAnime in vite config

--- vite.config.ts
+++ vite.config.ts
@@ -113,7 +113,7 @@ function figmaSiteConfiguration(config: FigmaSiteConfiguration): Plugin {
     return html.replace(`<!-- ${slotName} -->`, content)
   }
 
-  const title = config.title ?? "Figma Make App"
+  const title = config.title ?? "openAnime"
   const description = config.description ?? ''
   const favicon = config.icons?.icon ?? ''
   const socialImage = config.openGraph?.image ?? ''
